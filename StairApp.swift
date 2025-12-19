7a6ef11244104bc1f357ab5cb1ca76856223c44f
sagalilja307-jpg <sagalilja307@gmail.com>
2025-12-19 23:24:21 +0100

    Add files via upload

---
+++ StairApp.swift
@@ -0,0 +1,17 @@
+//
+//  StairApp.swift
+//  Stair
+//
+//  Created by Saga Lilja on 2025-12-18.
+//
+
+import SwiftUI
+
+@main
+struct StairApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
